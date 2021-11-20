ab34b2eea08dd30e836f5f88858e66b1ca16d405
odorifqi <09021281621065@student.unnsri.ac.id>
2021-11-20 23:54:00 +0700

    add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,18 @@ module.exports = {
           },
         ],
       },
+      // DELETE RULE CSS TO USE SASS, VICE VERSA
+      // {
+      //   test: /\.s[ac]ss$/i,
+      //   use: [
+      //     // Creates `style` nodes from JS strings
+      //     "style-loader",
+      //     // Translates CSS into CommonJS
+      //     "css-loader",
+      //     // Compiles Sass to CSS
+      //     "sass-loader",
+      //   ],
+      // },
     ],
   },
   resolve: {
