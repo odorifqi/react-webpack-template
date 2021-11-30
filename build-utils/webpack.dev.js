9f23d86e1168807a632aa2313ddf09064e6f9f64
odorifqi <09021281621065@student.unnsri.ac.id>
2021-11-30 17:31:07 +0700

    .env config

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-undef */
-const { DefinePlugin } = require("webpack");
+/* eslint-disable no-undef */
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
   plugins: [
-    new DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("development"),
-      },
+    new Dotenv({
+      path: path.resolve(__dirname, "..", "./.env.development"),
     }),
   ],
   devtool: "eval-source-map",
--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-undef */
-const { DefinePlugin } = require("webpack");
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   plugins: [
-    new DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production"),
-      },
+    new Dotenv({
+      path: path.resolve(__dirname, "..", "./.env.production"),
     }),
   ],
-  devtool: "source-map",
+  devtool: "eval-source-map",
 };
